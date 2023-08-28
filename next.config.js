a61d29805cd0fc126ac7951925dbe396cad92947
SynthoMind <bestSynthomind@gmail.com>
2023-08-28 09:42:36 +0330

    try2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
     reactStrictMode: true,
-    images: {
-        domains: ['example.com'], // Add the domains for your images
-    },
+
     // Any other configuration options you need
 };
